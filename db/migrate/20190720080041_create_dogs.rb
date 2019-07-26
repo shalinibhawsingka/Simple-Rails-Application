217910f23ab00df143b6fc79920fb3c61151124b
-g <-g>
2019-07-26 18:32:19 +0530

    validation added

--- db/migrate/20190720080041_create_dogs.rb
+++ db/migrate/20190720080041_create_dogs.rb
@@ -1,8 +1,8 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   def change
     create_table :dogs do |t|
-      t.string :name
-      t.string :email
+      t.string :name, null: false, unique: true
+      t.string :email, null: false, unique: true
 
       t.timestamps
     end
